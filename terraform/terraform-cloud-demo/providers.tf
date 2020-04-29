284aa1dfc1f73e255ba2a785e595f7c2a2f62b57
mfykmn <mfykmn@gmail.com>
2020-04-29 17:19:58 +0900

    Updated

--- terraform/terraform-cloud-demo/providers.tf
+++ terraform/terraform-cloud-demo/providers.tf
@@ -1,9 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 provider "aws" {
-  version    = "2.8"
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  version = "~> 2.8"
 }
